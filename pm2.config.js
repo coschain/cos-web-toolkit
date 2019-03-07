045bc2c4dc1734e92e00c1a6ae0358a55b6a454b
aprocysanae <axdiaoqi220@gmail.com>
2019-03-07 13:38:15 +0800

    add cos-grpc-js dependence

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps : [{
     name: 'API',
     script: 'app.js',
-
+    error_file: '/data/logs/cos-web-toolkit/error.log',
+    out_file : "./data/logs/cos-web-toolkit/std.out",
     instances: 1,
     autorestart: true,
     watch: false,

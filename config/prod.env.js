0a28ee7c95fa502498b1181911060fa42bb8eb94
aprocysanae <axdiaoqi220@gmail.com>
2019-03-20 13:28:41 +0800

    chain ip

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   // VUE_APP_CHAIN: '"https://34.230.216.41:8080"'
-  VUE_APP_CHAIN: '"https://testnode.contentos.io:8080"'
+  // VUE_APP_CHAIN: '"https://testnode.contentos.io:8080"'
+  VUE_APP_CHAIN: "'http://34.200.75.66:8080'"
 }

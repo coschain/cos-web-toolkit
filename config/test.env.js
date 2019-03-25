4f23d2a026fb6c816deaa6aaa23b8f1597bbcc43
aprocysanae <axdiaoqi220@gmail.com>
2019-03-25 15:10:07 +0800

    test env js

--- config/test.env.js
+++ config/test.env.js
@@ -1,9 +1,6 @@
 'use strict'
-const merge = require('webpack-merge')
-const devEnv = require('./dev.env')
-
-module.exports = merge(devEnv, {
+module.exports = {
   NODE_ENV: '"testing"',
   VUE_APP_CHAIN: '"https://testnode.contentos.io"',
   VUE_APP_FAUCET: true
-})
+}
